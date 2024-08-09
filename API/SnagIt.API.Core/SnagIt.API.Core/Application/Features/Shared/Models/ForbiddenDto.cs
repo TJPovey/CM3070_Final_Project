6af4b3072e44d87961758343eb63ce13d765fede
Tom Povey <Tom.Povey@ghd.com>
2024-08-09 18:19:11 +0100

    add authentication and middleware

---
+++ API/SnagIt.API.Core/SnagIt.API.Core/Application/Features/Shared/Models/ForbiddenDto.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+namespace SnagIt.API.Core.Application.Features.Shared.Models
+{
+    public class ForbiddenDto : Response
+    {
+        public override ResponseData Data { get; set; }
+
+    }
+}
--- API/SnagIt.API.Core/SnagIt.API.Core/Infrastructure/Extensions/HttpHeadersCollectionExtensions.cs
+++ API/SnagIt.API.Core/SnagIt.API.Core/Infrastructure/Extensions/HttpHeadersCollectionExtensions.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.Azure.Functions.Worker.Http;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnagIt.API.Core.Infrastructure.Extensions
 {
     public static class HttpHeadersCollectionExtensions
     {
+        public static string GetAuthorizationHeaderValue(this HttpHeadersCollection headersCollection)
+        {
+            var authToken = string.Empty;
+
+            if (headersCollection.TryGetValues("Authorization", out var authHeaderValue))
+            {
+                authToken = authHeaderValue.FirstOrDefault()?.Split(" ").Last() ?? string.Empty;
+            }
+
+            return authToken;
+        }
+
         public static ClaimsPrincipal GetClaimsPrincipal(this HttpHeadersCollection headersCollection)
         {
             var authToken = string.Empty;
